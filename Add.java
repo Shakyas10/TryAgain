531b4ecbb534ddadf5f38a1600035e8dc57c60d9
Shweta <shweta1shakya@gmail.com>
2020-08-16 15:20:12 +0530

    adding new files

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i = 2 , j = 3 , k;
+        k = i + j;
+        System.out.println("Addition of i and j gives.."+k);
+        
+	}
+
+}
---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+	public static void main(String[] args) {
+		int i =2;
+		int j =3;
+		int k = i+j;
+		System.out.println("Sum is=" +k);
+		
+		
+	}
+
+}
